a9eb9086898cd300dba2714d7c4abd6428b17cfb
arthurlamure01@gmail.com <arthurlamure01@gmail.com>
2020-02-19 17:55:26 +0100

    Start to implements viewDetailsEpisode onclick

--- FirstProject/src/screens/DetailsScreen.tsx
+++ FirstProject/src/screens/DetailsScreen.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import {Image, SafeAreaView, StatusBar, Text, View} from "react-native";
 
-import {Characters} from "../global/APIType";
+import {Characters, Episodes} from "../global/APIType";
 import {getCharacterById} from "../global/getCharacter";
-
+import {getEpisodesByIds} from "../global/getListOfEpisodes";
 
 //avec le props.navigation.push..... on envoie les données depuis l'autre screen
 const DetailsScreen = ({ route }) => {
     const [character, setCharacter] = React.useState<Characters>()
+    const [episodes, setEpisodes] = React.useState<Episodes[]>([])
     const { characterId } = route.params
+    let episodesIds = [];
 
     React.useEffect(() => {
-        getCharacterById(characterId).then(setCharacter)
+        getCharacterById(characterId).then((data) => {
+            setCharacter(data)
+            episodesIds =  data.episode.map(episode =>
+                parseInt(episode.replace('https://rickandmortyapi.com/api/episode/', "")))
+            getEpisodesByIds(episodesIds).then(setEpisodes)
+            //character?.episode.reduce(episode => episode.split('/').pop() //return last elem dans notre cas l'id
+            //episodesIds.push(parseInt(character?.episode.reduce(episode => episode.replace('https://rickandmortyapi.com/api/episode/', ""))));
+        })
     }, [])
 
     return (
@@ -24,11 +33,21 @@ const DetailsScreen = ({ route }) => {
                     <Text> {character?.status} </Text>
                     <Text> {character?.gender} </Text>
                     <Text> Episodes </Text>
-                    {
-                        character?.episode.map(episodeName =>
-                            <Text>
-                                {episodeName.replace('https://rickandmortyapi.com/api/episode/', "")}
-                            </Text>
+                    { // ici ce qu'on veut avoir c'est quand on click sur un episode le details apparait dessous
+                        character?.episode.map(episodeName =>{
+                            let show = false
+                                return (
+                                    <Text key={episodeName} onPress={() => show = !show}>
+                                        {
+                                            show ?
+                                                episodeName.replace('https://rickandmortyapi.com/api/episode/', "")
+                                                :
+                                                null
+                                        }
+                                    </Text>
+                                )
+                        }
+
                         )
                     }
 
--- FirstProject/src/screens/HomeScreen.tsx
+++ FirstProject/src/screens/HomeScreen.tsx
@@ -24,6 +24,7 @@ import {getCharacters} from "../global/getCharacters";
 import {CharacterList} from "../components/CharacterList";
 import {NavigationButton} from "../components/NavigationButton";
 import {SearchBar} from "../components/SearchBar";
+import {getEpisodesByIds} from "../global/getListOfEpisodes";
 
 
 const HomeScreen = (props) => {
